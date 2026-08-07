62ea5fb191bafb3402f4eed5db7c041edcc7500c
QuanticSolutions <qsuidesigner@gmail.com>
2026-08-07 12:38:48 +0500

    Update HeroSlider.jsx

--- src/components/home/HeroSlider.jsx
+++ src/components/home/HeroSlider.jsx
@@ -10,34 +10,34 @@ export function InfiniteMovingCardsDemo() {
 
 // Replace testimonials with test images
 const testImages = [
-  {
-    src: "wordpress/Fintalk.jpg",
-    alt: "Test Image 1",
-    name: "Fintalk",
-    link: "#"
-  },
-  {
-    src: "/assets/projects/web/Shopify/1.jpg",
-    alt: "Test Image 2",
-    name: "Pop Charm",
-    link: "https://popcharmshop.com/"
-  },
-  {
-    src: "/assets/projects/web/Custom/3.jpg",
-    alt: "Test Image 3",
-    name: "Cars Finder Pro",
-    link: "https://carsfinderpro.com/"
-  },
-  {
-    src: "/assets/projects/web/Wordpress/7.jpg",
-    alt: "Test Image 4",
-    name: "Aussie Crops",
-    link: "https://theaussiecrops.com.au/"
-  },
-  {
-    src: "/assets/projects/web/Wordpress/17.jpg",
-    alt: "Test Image 5",
-    name: "NGO Loob",
-    link: "https://loobinc.com/"
-  },
+  // {
+  //   src: "wordpress/Fintalk.jpg",
+  //   alt: "Test Image 1",
+  //   name: "Fintalk",
+  //   link: "#"
+  // },
+  // {
+  //   src: "/assets/projects/web/Shopify/1.jpg",
+  //   alt: "Test Image 2",
+  //   name: "Pop Charm",
+  //   link: "https://popcharmshop.com/"
+  // },
+  // {
+  //   src: "/assets/projects/web/Custom/3.jpg",
+  //   alt: "Test Image 3",
+  //   name: "Cars Finder Pro",
+  //   link: "https://carsfinderpro.com/"
+  // },
+  // {
+  //   src: "/assets/projects/web/Wordpress/7.jpg",
+  //   alt: "Test Image 4",
+  //   name: "Aussie Crops",
+  //   link: "https://theaussiecrops.com.au/"
+  // },
+  // {
+  //   src: "/assets/projects/web/Wordpress/17.jpg",
+  //   alt: "Test Image 5",
+  //   name: "NGO Loob",
+  //   link: "https://loobinc.com/"
+  // },
 ];
